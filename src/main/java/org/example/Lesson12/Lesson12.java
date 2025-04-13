e2d5b0056b5d718f0b09622bd632e91bdd19e292
Mad-in-red <abilibenko@gmail.com>
2025-04-14 00:49:49 +0300

    Доработка дз к уроку 12 AQA

--- src/main/java/org/example/Lesson12/Lesson12.java
+++ src/main/java/org/example/Lesson12/Lesson12.java
@@ -2,23 +2,23 @@ package org.example.Lesson12;
 
 public class Lesson12 {
     public static void main(String[] args) {
-        ArraysMethods array1 = new ArraysMethods();
-        String[][] arrayOne = {
+        ArraysMethods arraysMethods = new ArraysMethods();
+
+        String[][] array1 = {
                 {"2", "1", "3", "4", "89"},
                 {"5", "6", "7", "8"},
-                {"9", "text", "11", "12"},
+                {"9", "23", "11", "12", "12"},
                 {"13", "14", "15", "16"}
         };
 
         try {
-            array1.creatingArray(arrayOne);
-        } catch (MyArraySizeException e) {
+            System.out.println("Сумма: " + arraysMethods.checkSizeAndSummArray(array1));
+        } catch (MyArraySizeException | MyArrayDataException e) {
             System.err.println(e.getMessage());
 
         }
 
-        ArraysMethods array2 = new ArraysMethods();
-        String[][] arrayTwo = {
+        String[][] array2 = {
                 {"2", "1", "3", "4"},
                 {"5", "6", "7", "8"},
                 {"9", "text", "11", "12"},
@@ -26,14 +26,12 @@ public class Lesson12 {
         };
 
         try {
-            int sum = array2.sumArray(arrayTwo);
-            System.out.println("Сумма элементов массива: " + sum);
+            System.out.println("Сумма: " + arraysMethods.checkSizeAndSummArray(array2));
         } catch (MyArraySizeException | MyArrayDataException e) {
             System.out.println(e.getMessage());
         }
 
-        ArraysMethods array3 = new ArraysMethods();
-        String[][] arrayTree = {
+        String[][] array3 = {
                 {"2", "1", "3", "4"},
                 {"5", "6", "7", "8"},
                 {"9", "152", "11", "12"},
@@ -41,22 +39,18 @@ public class Lesson12 {
         };
 
         try {
-            System.out.println(arrayOne[4][0]);
-        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("Сумма: " + arraysMethods.checkSizeAndSummArray(array3));
+        } catch (MyArraySizeException | MyArrayDataException e) {
             System.out.println("Ошибка: " + e.getMessage());
         }
 
         try {
-            int sum = array3.sumArray(arrayTree);
-            System.out.println("Сумма элементов массива: " + sum);
-        } catch (MyArraySizeException | MyArrayDataException e) {
+            String[][] array4 = new String[4][4];
+            System.out.println(array4[4][0]);
+        } catch (ArrayIndexOutOfBoundsException e) {
             System.out.println(e.getMessage());
         }
     }
-
-
-
-
 
 
 }
